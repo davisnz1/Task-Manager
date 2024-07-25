0debc7830d55f76f7032f4d0b7fe8796a89311b2
davisnz1 <davissouza2021@gmail.com>
2024-07-24 22:52:43 -0300

    estilizei os button da sidebar!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
